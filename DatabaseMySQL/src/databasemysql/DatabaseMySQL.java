e5cf860bf593f7794bfea3ef91831eacb5748ea1
johnny1212 <johnny1212@volny.cz>
2013-11-16 20:10:08 +0100

    tvorba - Lineární seznam

--- DatabaseMySQL/src/databasemysql/DatabaseMySQL.java
+++ DatabaseMySQL/src/databasemysql/DatabaseMySQL.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package databasemysql;
 
 import MySQL.MySQL;
@@ -26,6 +22,5 @@ public class DatabaseMySQL {
         } catch (Exception e) {
             System.out.println("err");
         }
-
     }
 }
